b9fd8f07701f3be6caaa857325a5a1af0b008ed4
Igor Gurianov <igor.gurianov@inbox.ru>
2022-12-28 23:57:46 +0500

    Feature: tablet markup added

---
+++ script-js/swiper-services.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper-services', {
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 5
+    },
+
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 7,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
---
+++ script-js/swiper.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper-experts', {
+  // Optional parameters
+  grabCoursor: true,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    }
+  },
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+}
+);
