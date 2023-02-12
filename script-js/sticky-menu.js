7f8be8220026644fa5e74114907a850d64ccee89
Igor Gurianov <igor.gurianov@inbox.ru>
2023-02-12 17:12:52 +0500

    Feat: sections

--- script-js/sticky-menu.js
+++ script-js/sticky-menu.js
@@ -2,18 +2,18 @@ window.onscroll = function () { myFunction() };
 console.log()
 
 const header = document.getElementById("myHeader");
-const sectionInterview = document.querySelector('.interview')
-console.log(sectionInterview)
+const sectionContent = document.querySelector('.content')
+console.log(sectionContent)
 
 var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.innerWidth > 768 && window.pageYOffset > sticky) {
     header.classList.add("nav-bar_position-type_sticky");
-    sectionInterview.classList.add("interview_position_type_sticky");
+    sectionContent.classList.add("content_position_type_sticky");
   } else {
     header.classList.remove("nav-bar_position-type_sticky");
-    sectionInterview.classList.remove("interview_position_type_sticky");
+    sectionContent.classList.remove("content_position_type_sticky");
   }
 }
 
